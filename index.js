accad69a57690102cc0fcda98be15200a107bff6
Winicius Machado Castanha <winiicastanha@gmail.com>
2024-10-10 11:08:45 -0300

    Adicionando a funcionalidade de gerar curriculo

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/certificados', express.static(path.join(__dirname, 'certificados')));
+app.use('/curriculos', express.static(path.join(__dirname, 'curriculos')));
 
-async function gerarPDF(nomeAluno, mes, ano, cpf) {
+async function gerarPDFCertificado(nomeAluno, mes, ano, cpf) {
     try {
         const html = await ejs.renderFile(path.join(__dirname, 'conteudoCertificado.ejs'), {
             nomeAluno, mes, ano
@@ -47,6 +48,40 @@ async function gerarPDF(nomeAluno, mes, ano, cpf) {
     }
 }
 
+async function gerarPDFCurriculo(dadosCurriculo, cpf) {
+    try {
+        const html = await ejs.renderFile(path.join(__dirname, 'conteudoCurriculo.ejs'), dadosCurriculo);
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.setContent(html, { waitUntil: 'networkidle0' });
+
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            printBackground: true,
+            margin: {bottom: "20px", top: "60px", right: "20px", left: "60px"},
+        });
+
+        await browser.close();
+
+
+        const filePath = path.join(__dirname, 'curriculos', `${cpf}-curriculo.pdf`);
+
+        if (!fs.existsSync(path.join(__dirname, 'curriculos'))) {
+            fs.mkdirSync(path.join(__dirname, 'curriculos'));
+        }
+
+        fs.writeFileSync(filePath, pdfBuffer);
+
+        return filePath;
+
+    } catch (error) {
+        console.error('Erro ao gerar o PDF do currículo:', error);
+        throw error;
+    }
+}
+
 app.post('/gerar-certificado', async (req, res) => {
     const { nomeAluno, mes, ano, cpf } = req.body;
 
@@ -55,10 +90,8 @@ app.post('/gerar-certificado', async (req, res) => {
     }
 
     try {
-        const filePath = await gerarPDF(nomeAluno, mes, ano, cpf);
-
+        const filePath = await gerarPDFCertificado(nomeAluno, mes, ano, cpf);
         const fileUrl = `${req.protocol}://${req.get('host')}/certificados/${path.basename(filePath)}`;
-
         return res.json({ url: fileUrl });
 
     } catch (error) {
@@ -67,6 +100,45 @@ app.post('/gerar-certificado', async (req, res) => {
     }
 });
 
+
+app.post('/gerar-curriculo', async (req, res) => {
+    const { nomeAluno, dataNascimento, rua, numero, cidade, uf, complemento, telefone, telefoneRecado, email, objetivos, caracteristicas, escolaridade, experienciaProfissional, formacaoComplementar, trabalhoVoluntario, idiomas, cpf } = req.body;
+
+    if (!nomeAluno || !cpf) {
+        return res.status(400).send("Parâmetros obrigatórios ausentes");
+    }
+
+    const dadosCurriculo = {
+        nomeAluno,
+        dataNascimento,
+        rua,
+        numero,
+        cidade,
+        uf,
+        complemento,
+        telefone,
+        telefoneRecado,
+        email,
+        objetivos,
+        caracteristicas,
+        escolaridade,
+        experienciaProfissional,
+        formacaoComplementar,
+        trabalhoVoluntario,
+        idiomas
+    };
+
+    try {
+        const filePath = await gerarPDFCurriculo(dadosCurriculo, cpf);
+        const fileUrl = `${req.protocol}://${req.get('host')}/curriculos/${path.basename(filePath)}`;
+        return res.json({ url: fileUrl });
+
+    } catch (error) {
+        console.error('Erro ao gerar o PDF do currículo:', error);
+        res.status(500).send('Erro ao gerar o PDF do currículo');
+    }
+});
+
 app.listen(3500, () => {
-    console.log('Servidor rodando na porta 3000');
+    console.log('Servidor rodando na porta 3500');
 });
