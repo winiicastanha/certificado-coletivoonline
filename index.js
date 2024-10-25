a4b4af19d3a884710233d092a8caa5ee62188137
Gabriel Souza <skolino90@gmail.com>
2024-10-25 00:04:26 -0300

    Ajuste cors

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./src/routes/routes');
+const cors = require('cors');
 
 const app = express();
+
+// Permite CORS para todas as origens
+app.use(cors());
+
 app.use(bodyParser.json());
 
+// Servir arquivos estáticos da pasta 'documentos'
 app.use('/documentos', express.static(path.join(__dirname, 'documentos')));
 
+// Usar as rotas configuradas no arquivo routes.js
 app.use('/', routes);
 
+// Iniciar o servidor na porta 3500
 app.listen(3500, () => {
     console.log('Servidor rodando na porta 3500');
 });
